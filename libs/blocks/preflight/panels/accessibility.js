058563764c06867a90bcaee6a1b2d74c5df81ad9
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2023-09-27 11:57:40 -0700

    Update accessibility.js

--- libs/blocks/preflight/panels/accessibility.js
+++ libs/blocks/preflight/panels/accessibility.js
@@ -1,31 +1,56 @@
 import { html, signal, useEffect } from '../../../deps/htm-preact.js';
-const content = signal({});
 
+const DEF_ICON = 'purple';
+const DEF_DESC = 'Checking...';
+const pass = 'green';
+const fail = 'red';
+
+const content = signal({});
+const altResult = signal({ icon: DEF_ICON, title: 'Image alt value', description: DEF_DESC });
 
 async function checkAlt() {
   const images = document.querySelectorAll('img');
   const imagesWithoutAlt = [];
+  const result = { ...altResult.value };
   images.forEach((img) => {
-    //let imageDetail = {};
     const alt = img.getAttribute('alt');
     if (!alt || alt.trim() === '') {
-      //imageDetail = { img.getAttribute('src').split('?')[0] };
       imagesWithoutAlt.push(img.getAttribute('src').split('?')[0]);
     }
   });
   if (!imagesWithoutAlt.length) {
-    imagesWithoutAlt.push('All images are valid.');
+    result.icon = pass;
+    result.description = 'Reason: All Image are valid';
+  } else {
+    result.icon = fail;
+    result.description = 'Reason: Alt attribute and values are missing for one or more images on the page';
   }
   content.value = imagesWithoutAlt;
-  Object.keys(content.value).map((key) => console.log(content.value[key]));
+  altResult.value = result;
+  return result.icon;
+}
+
+function AccessibilityItem({ icon, title, description }) {
+  return html`
+    <div class=seo-item>
+      <div class="result-icon ${icon}"></div>
+      <div class=seo-item-text>
+        <p class=seo-item-title>${title}</p>
+        <p class=seo-item-description>${description}</p>
+      </div>
+    </div>`;
 }
 
 export default function Accessibility() {
   useEffect(() => { checkAlt(); }, []);
 
   return html`
+  <div class=seo-column>
+        <${AccessibilityItem} icon=${altResult.value.icon} title=${altResult.value.title} description=${altResult.value.description} />
+    </div>
   <div>
-  <table border='1|1'>
+  ${content.value.length > 0 && html`
+  <table border='1|1' width=100%>
     <thead>
     <th>Image Src</th>
     </thead>
@@ -35,5 +60,6 @@ export default function Accessibility() {
     `)}
     </tbody>
     </table>
+    `}
   </div>`;
 }
